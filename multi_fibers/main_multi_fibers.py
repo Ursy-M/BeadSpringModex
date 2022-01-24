ee4723dd5ec8a8f27058217ac665b2a971b5e4e8
Ursy-M <makanga@naruto.local>
2022-01-24 17:18:53 +0100

    new implementation

---
+++ multi_fibers/main_multi_fibers.py
@@ -0,0 +1,211 @@
+
+"""
+main file to run a simultion with n fibers
+"""
+
+import numpy as np
+import argparse
+from scipy.integrate import ode
+from functools import partial
+from datetime import datetime
+
+
+# =============================================================================
+# import project functions
+# =============================================================================
+from read_input import read_input
+from read_input import read_clone_file
+from read_input import read_vertex_file_list
+from fiber import fiber
+from integrator.integrator import Integrator
+from force import force
+
+# =============================================================================
+# define modules
+# =============================================================================
+def get_beads_r_vectors(bodies_fibers, number_of_beads):
+    """
+    Parameters
+    ----------
+    bodies_fibers : TYPE
+        DESCRIPTION.
+    number_of_beads : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    r_vectors : TYPE
+        DESCRIPTION.
+
+    """
+    r_vectors = np.empty((number_of_beads, 3))
+    offset = 0
+    for x in bodies_fibers:
+        NF = x.NF
+        r_vectors[offset:(offset + NF)] = x.get_fiber_r_vectors()       
+        offset += NF
+    
+    return r_vectors
+
+
+def get_beads_list_indices(bodies_fibers):
+    """
+    Parameters
+    ----------
+    bodies_fibers : TYPE
+        DESCRIPTION.
+        
+    Returns
+    -------
+    bead_list_index : TYPE
+        DESCRIPTION.
+
+    """
+    bead_list_index = []
+    for k, f in enumerate(bodies_fibers):
+        NF = f.NF
+        bead_list_index.append(k*np.ones(NF, dtype=int))
+    
+    
+    bead_list_index = [item for sublist in bead_list_index for item in sublist]
+    
+    return bead_list_index
+
+
+# =============================================================================
+# main
+# =============================================================================
+# get command line
+parser = argparse.ArgumentParser(description='run a multi_fibers_and_obstacles simulation and save trajectory.')
+parser.add_argument('--input-file', dest='input_file', type=str, default='data.main', help='name of the input file')
+args = parser.parse_args()
+input_file = args.input_file
+
+# read the input file
+read = read_input.ReadInput(input_file)
+
+# set some useful entries for the simualtion
+n_steps = read.n_steps
+initial_step = read.initial_step
+n_save = read.n_save
+dt = read.dt
+eta = read.eta
+W = read.weight_per_unit_length
+B = read.elasto_gravitational_number
+a_bead_fib = read.fiber_bead_radius
+DL_factor = read.centerline_distance_factor
+output_name = read.output_name
+fibers = read.fibers
+fibers_ID = read.fibers_ID
+
+# set some implementations
+force.calc_steric_forces = force.set_steric_forces(read.set_steric_forces_implementation)
+
+
+body_types = []
+body_names = []
+bodies_fibers = []
+# create fibers 
+for ID, fib in enumerate(fibers):
+    print('creating fibers =', fib[1])
+    # read vertex and clones files
+    number_of_beads_in_fiber = read_vertex_file_list.read_vertex_file_list(fib[0])   
+    number_of_beads_all, fiber_bead_locations = read_clone_file.read_clone_file(fib[1])
+    body_types.append(number_of_beads_all // number_of_beads_in_fiber)
+    body_names.append(fibers_ID[ID])
+    # create each body of type fiber
+    for i in range(number_of_beads_all // number_of_beads_in_fiber):
+        f = fiber.Fiber(fiber_bead_locations[i*number_of_beads_in_fiber:(i+1)*number_of_beads_in_fiber,:],
+                        DL_factor,
+                        a_bead_fib)
+        f.ID = fibers_ID[ID]
+        # append fiber to the total bodies list
+        bodies_fibers.append(f)
+
+        
+# set some variables
+number_types_fiber = len(body_types)
+bodies_fibers = np.array(bodies_fibers)
+number_bodies_fiber = len(bodies_fibers)
+number_of_beads_fiber = sum(x.NF for x in bodies_fibers)
+number_of_beads = int(number_of_beads_fiber)
+radius_array = np.ones((number_of_beads))
+radius_array[0:number_of_beads_fiber] *= a_bead_fib
+bead_list_index = get_beads_list_indices(bodies_fibers) 
+# initial values        
+r_vectors = get_beads_r_vectors(bodies_fibers, number_of_beads)
+initial_values = np.squeeze(np.asarray(r_vectors.reshape(1, 3*number_of_beads)))
+
+# save system information
+with open(output_name + '.system_info', 'w') as g:
+    g.write('number_of_fiber_types     ' + str(number_types_fiber) + '\n')
+    g.write('body_names                ' + str(body_names) + '\n')
+    g.write('number_of_fibers          ' + str(number_bodies_fiber) + '\n')
+    g.write('number_of_beads           ' + str(number_of_beads) + '\n')
+
+g.close()
+
+
+# create integrator
+integrator = Integrator(bodies_fibers, 
+                        number_of_beads, 
+                        scheme=read.scheme, 
+                        domain=read.domain,
+                        mobility_vector_prod_implementation=read.mobility_vector_prod_implementation)
+
+integrator.calc_gravitational_forces     = partial(force.calc_gravitational_forces,
+                                                   weight_per_unit_length=W)
+
+integrator.calc_steric_forces            = partial(force.calc_steric_forces,
+                                                   periodic_length=read.periodic_length,
+                                                   repulsion_strength=read.repulsion_strength,
+                                                   stiffness_parameter=read.stiffness_paramater,
+                                                   contact_distance_factor=read.contact_distance_factor,
+                                                   radius_array=radius_array,
+                                                   bead_list_index=bead_list_index)
+
+integrator.calc_stretching_forces        = partial(force.calc_stretching_forces,
+                                                   weight_per_unit_length=W,
+                                                   elasto_gravitational_number=B)           
+
+integrator.calc_bending_forces           = partial(force.calc_bending_forces,
+                                                   weight_per_unit_length=W,
+                                                   elasto_gravitational_number=B)
+
+integrator.get_beads_r_vectors           = get_beads_r_vectors
+integrator.eta                           = eta
+integrator.a_bead_fib                    = a_bead_fib
+integrator.periodic_length               = read.periodic_length
+integrator.weight_per_unit_length        = W
+integrator.number_of_beads_fiber         = number_of_beads_fiber
+
+
+# ode solver
+t = np.arange(initial_step, n_steps, dt)                                
+r = ode(integrator)
+r.set_integrator('vode', method=read.scheme, with_jacobian=True)
+r.set_initial_value(initial_values, initial_step)
+sol = np.zeros((len(t), len(initial_values)))
+all_t = np.zeros((len(t), 1))
+idx = 0
+
+# loop over time steps
+start_time = datetime.now()
+while r.successful() and r.t < t[-1]:
+    print('integrator = ', read.scheme, ', step = ', idx,  ', t + dt = ', r.t + dt)
+    sol[idx, :] = r.y
+    all_t[idx, :] = t[idx]
+    r.integrate(r.t + dt)
+    idx += 1
+
+
+# save
+np.savetxt(output_name + ".output_positions.csv", sol, delimiter=';')
+np.savetxt(output_name + ".output_times.csv", all_t, delimiter=';')
+
+    
+duration = datetime.now() - start_time
+print('time :\t' + 'execution\t' + str(duration))
+print('\n\n\n# End')
+       
+
---
+++ multi_fibers/postprocessor_illustration.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 14:44:46 2020
+
+@author: Ursy
+"""
+
+import sys
+import numpy as np
+import os
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FFMpegWriter
+from matplotlib import rc
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+rc('font',**{'family':'serif','serif':['Times']})
+rc('text', usetex=True)
+
+plt.rc('xtick',labelsize=20)
+plt.rc('ytick',labelsize=20)
+params = {'legend.fontsize': 20,
+          'legend.handlelength': 2}
+plt.rcParams.update(params)
+
+plt.rcParams['animation.ffmpeg_path'] = '/usr/local/bin/ffmpeg';
+
+# =============================================================================
+# define some useful functions
+# =============================================================================
+def set_axes_equal(ax):
+    
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+
+# =============================================================================
+# main
+# =============================================================================
+path = 'output'     
+current_directory = os.getcwd()
+directory = current_directory + '/' + path + '/'
+
+
+# set some entries
+# fiber bead radius 
+a = 1e-03                                              
+# number of fiber beads                                                    
+Nb = 20
+# length                                 
+LS = Nb*2.2*a
+# viscosity
+eta = 1.0
+# weight per unit length                                                     
+W = 1.0
+# number of fibers
+n_fibers = 1
+
+# setling time
+unit_time_gravity = LS*eta/W
+
+# load ouput data
+y = np.loadtxt(directory + "run.output_positions.csv", delimiter=";")
+tk = np.loadtxt(directory + "run.output_times.csv", delimiter=";")
+# get number of time step
+TOTAL_STEPS = y.shape[0]
+ 
+plot_3d = False
+# plot setup
+plot_steps = 30                                            
+plot_now = plot_steps - 1
+if plot_3d==False:
+    fig, ax = plt.subplots(figsize=(7, 7))
+elif plot_3d==True:
+    fig = plt.figure()
+    ax  = plt.axes(projection='3d')
+plt.subplots_adjust(bottom=0.25)
+
+
+# Movie setup
+np.random.seed(2984)
+metadata=dict(title="Sedimentation",artist="Low_Reynolds_Number",comment="p");
+writer=FFMpegWriter(fps=2,metadata=metadata)
+
+
+Lim = 0.5
+
+
+# build the movie 
+with writer.saving(fig, directory + "Movie.mov",100):
+    for k in range(TOTAL_STEPS - 1):
+        plot_now = plot_now + 1
+        
+        all_x = list()
+        all_y = list()
+        all_z = list()
+        
+        Ny = int(y.shape[1]/3)
+        if (k==0):
+            for kk in range(Ny) :
+                all_x.append(y[:,(3*kk)][0])
+                all_y.append(y[:,(3*kk+1)][0])
+                all_z.append(y[:,3*kk+2][0])
+                
+            all_x[:] = [x/LS for x in all_x]
+            all_y[:] = [y/LS for y in all_y]
+            all_z[:] = [z/LS for z in all_z]
+            x_m = np.mean(all_x)
+            y_m = np.mean(all_y)
+            z_m = np.mean(all_z)
+            
+        else :
+            for kk in range(Ny) :
+                all_x.append(y[:,(3*kk)][k])
+                all_y.append(y[:,(3*kk+1)][k])
+                all_z.append(y[:,3*kk+2][k])
+                
+            all_x[:] = [x/LS for x in all_x]
+            all_y[:] = [y/LS for y in all_y]
+            all_z[:] = [z/LS for z in all_z]
+            x_m = np.mean(all_x)
+            y_m = np.mean(all_y)
+            z_m = np.mean(all_z)
+            
+        if (plot_now == plot_steps):
+            if plot_3d==False:
+                if n_fibers==1:
+                    ax.plot((all_x - x_m),(all_z - z_m),'-')
+                elif n_fibers==2:
+                    ax.plot((all_x[:Nb] - x_m),(all_z[:Nb] - z_m),'-')
+                    ax.plot((all_x[Nb:] - x_m),(all_z[Nb:] - z_m),'-')
+            elif plot_3d==True:
+                if n_fibers==1:
+                    ax.plot3D((all_x - x_m), (all_y - y_m), (all_z - z_m),color='gray', linestyle='-')
+                elif n_fibers==2:
+                    ax.plot3D((all_x[:Nb] - x_m), (all_y[:Nb] - y_m), (all_z[:Nb] - z_m),color='gray', linestyle='-')
+                    ax.plot3D((all_x[Nb:] - x_m), (all_y[Nb:] - y_m), (all_z[Nb:] - z_m),color='gray', linestyle='-')
+                
+            # plot spheres for fiber and set figure parameters
+            if plot_3d==False:
+                for ii in range(Ny):
+                    max_step = 2*np.pi
+                    step = np.pi/100
+                    theta = np.arange(0, max_step, step)
+                    X0c = all_x[ii] - x_m
+                    Z0c = all_z[ii] - z_m
+                    Xc = X0c + (a/LS)*np.cos(theta)
+                    Zc = Z0c + (a/LS)*np.sin(theta)
+                    
+                    ax.plot(Xc, Zc, linestyle ='-', color = 'gray')
+                    ax.set_aspect('equal')
+                
+                ax.set_xlim(-Lim , Lim)
+                ax.set_ylim(-Lim, Lim)
+                ax.set_xticks([-Lim, 0, Lim])
+                ax.set_yticks([-Lim, 0, Lim])
+                ax.set_xlabel(r'$(x - x_{mean})/L$', fontsize=20)
+                ax.set_ylabel(r'$(z - z_{mean})/L$', fontsize=20)
+                ax.tick_params(axis = 'x', which='major', direction = 'in', top = True)
+                ax.tick_params(axis = 'x', which='minor', direction = 'in', top = True)
+                ax.tick_params(axis = 'y', which='major', direction = 'in', right = True)
+                ax.tick_params(axis = 'y', which='minor', direction = 'in', right = True)
+                #ax.set_title(r'$t/T=$' + str(int(tk[k]/unit_time_gravity)), fontsize=20)
+            elif plot_3d==True:
+                for ii in range(Ny):
+                    max_step_theta = 2*np.pi
+                    max_step_phi = np.pi
+                    N = 50
+                    stride=2
+                    theta = np.linspace(0, max_step_theta, N)
+                    phi = np.linspace(0, max_step_phi, N)
+                    
+                    X0co = all_x[ii] - x_m
+                    Y0co = all_y[ii] - y_m
+                    Z0co = all_z[ii] - z_m
+                    
+                    Xco = X0co + (a/LS)*np.outer(np.cos(theta), np.sin(phi))
+                    Yco = Y0co + (a/LS)*np.outer(np.sin(theta), np.sin(phi))
+                    Zco = Z0co + (a/LS)*np.outer(np.ones(np.size(theta)), np.cos(phi))
+                
+                    ax.plot_surface(Xco, Yco, Zco, linewidth=0.0, cstride=stride, \
+                                    rstride=stride, color='gray')                        
+                    set_axes_equal(ax)
+                
+                ax.set_xlim(-2*Lim , 2*Lim)
+                ax.set_ylim(-2*Lim, 2*Lim)
+                ax.set_zlim(-2*Lim, 2*Lim)
+                ax.set_xticks([-2*Lim, 0, 2*Lim])
+                ax.set_xticks([-2*Lim, 0, 2*Lim])
+                ax.set_zticks([-2*Lim, 0, 2*Lim])
+                ax.set_xlabel(r'$(x - x_{mean})/L$', fontsize=10)
+                ax.set_ylabel(r'$(y - y_{mean})/L$', fontsize=10)
+                ax.set_zlabel(r'$(z - z_{mean})/L$', fontsize=10)
+                
+            
+            plt.draw()
+            plt.pause(0.02)
+            
+
+            writer.grab_frame()
+            plt.cla()
+            
+        
+        if (plot_now == plot_steps):
+            plot_now = 0
+            
